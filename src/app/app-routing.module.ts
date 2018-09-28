da883320ac5a98f573f8c9e94fb0aabc5e91a8fe
O S <vystoya@gmail.com>
2018-09-28 16:37:18 +0300

    bug fixes on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
-import { ExamComponent } from './exam/exam.component';
-import { NewStudentComponent } from './new-student/new-student.component';
-import { StudentsComponent } from './students/students.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ResultsComponent } from './results/results.component';
-import { EnterResultsComponent } from './enter-results/enter-results.component';
+import { ExamComponent } from "./exam/exam.component";
+import { NewStudentComponent } from "./new-student/new-student.component";
+import { StudentsComponent } from "./students/students.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ResultsComponent } from "./results/results.component";
+import { EnterResultsComponent } from "./enter-results/enter-results.component";
 
-   
 const routes: Routes = [
-  { path: '', component: StudentsComponent },
-  { path: 'exams', component: ExamComponent },
-  { path: 'results', component: ResultsComponent },
-  { path: 'enter-results', component: EnterResultsComponent },
-  { path: 'students', component: StudentsComponent },
-  { path: 'students/register', component: NewStudentComponent }
+  { path: "", component: StudentsComponent },
+  { path: "exams", component: ExamComponent },
+  { path: "results", component: ResultsComponent },
+  { path: "enter-results", component: EnterResultsComponent },
+  { path: "students", component: StudentsComponent },
+  { path: "students/register", component: NewStudentComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ChartModule } from 'angular2-highcharts';  
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ChartModule } from "angular2-highcharts";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
-import { AppComponent } from './app.component';
-import { ResultsComponent } from './results/results.component';
-import { AppRoutingModule } from './app-routing.module';
-import { EnterResultsComponent } from './enter-results/enter-results.component';
-import { StudentsComponent } from './students/students.component';
-import { NewStudentComponent } from './new-student/new-student.component';
-import { ExamComponent } from './exam/exam.component';
+import { AppComponent } from "./app.component";
+import { ResultsComponent } from "./results/results.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { EnterResultsComponent } from "./enter-results/enter-results.component";
+import { StudentsComponent } from "./students/students.component";
+import { NewStudentComponent } from "./new-student/new-student.component";
+import { ExamComponent } from "./exam/exam.component";
 
 @NgModule({
   declarations: [
@@ -27,9 +28,9 @@ import { ExamComponent } from './exam/exam.component';
     // import HttpClientModule after BrowserModule.
     HttpClientModule,
     AppRoutingModule,
-    ChartModule.forRoot(require('highcharts'),
+    ChartModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
